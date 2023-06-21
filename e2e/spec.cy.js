325454963a7ad8e6931b3fb9824a281d0027d6f2
dhiogobn <dhiogo.nobrega@maisunifacisa.com>
2023-06-21 17:57:09 -0300

    informações complementares

--- e2e/spec.cy.js
+++ e2e/spec.cy.js
@@ -1,4 +1,5 @@
 const { timeout } = require("async")
+const { elementAt } = require("rxjs")
 
 describe('template spec', () => {
   it('passes', () => {
@@ -10,7 +11,7 @@ describe('template spec', () => {
     cy.get('#session_key').type('lticontasc@gmail.com')
     cy.get('#session_password').type('Linkedinlti!')
     cy.get('.justify-between > .btn-md').click()
-    cy.wait(10000)
+    cy.wait(20000)
     cy.visit('https://www.linkedin.com/in/micheleprofmatematica/')  
     cy.wait(2000)
     cy.get('.text-heading-xlarge').invoke('text').then((text) => {
@@ -54,20 +55,47 @@ describe('template spec', () => {
       console.log(lenguages[0].children[0].innerText)
     })
 
-    // cy.get('#top-card-text-details-contact-info').click()
-    // console.log("informações de contato")
+    cy.wait(2000)
+    cy.visit('https://www.linkedin.com/in/micheleprofmatematica/')  
+
+    cy.wait(2000)
+
+    cy.get('#top-card-text-details-contact-info').click()
+    console.log("informações de contato")
 
     // cy.get('.ci-vanity-url > .pv-contact-info__ci-container > .pv-contact-info__contact-link').invoke('text').then((text) => {
     //   const linkedinProfile = text.trim();
     //   console.log("perfil: ", linkedinProfile) 
     // })
     // const cont = 0;
-    // const array = []
-    // cy.get('.pv-contact-info__ci-container').each((sites) => {
-    //   if(sites[0].children[0].innerText) {
-    //     array.push(sites[0].children[0].innerText)
-    //   }
-    // });
+    const array = []
+    cy.get('.pv-contact-info__ci-container').each((sites) => {
+      if(sites[0].children[0].innerText) {
+        array.push(sites[0].children[0].innerText)
+      }
+    }).then((array) => {
+      array.each((element) => {
+        console.log("site: ", array[element].innerText)
+      })
+    });
+
+    cy.visit('https://www.linkedin.com/in/micheleprofmatematica/')  
+    cy.wait(2000)
+
+    cy.get('#ember134 > :nth-child(3) > :nth-child(1)').then((text) => {
+      const academicsFormations = text[0].innerText.trim();
+      console.log("formações academicas", academicsFormations)
+    })
+
+    cy.get('#ember114 > :nth-child(3) > :nth-child(1)').then((text) => {
+      const experiences = text[0].innerText.trim()
+      console.log("Experiencias: ", experiences)
+    })
+
+    cy.get('#ember233 > :nth-child(3) > :nth-child(1)').then((text) =>{
+      const competences = text[0].innerText.trim();
+      console.log("competencias", competences)
+    })
     })
     // console.log("sites: ")
     // cy.get(':nth-child(1) > .pv-contact-info__contact-link').invoke('text').then((text) => {
