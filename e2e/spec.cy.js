78829ce781f02760142d3628735bb2ccea84ba98
dhiogobn <dhiogo.nobrega@maisunifacisa.com>
2023-06-26 17:36:25 -0300

    scraper finalizado

--- e2e/spec.cy.js
+++ e2e/spec.cy.js
@@ -1,10 +1,10 @@
 const { timeout } = require("async")
 const { elementAt } = require("rxjs")
 
-const profiles = ['https://www.linkedin.com/in/micheleprofmatematica/', 'https://www.linkedin.com/in/dhiogo-bandeira-nobrega-660951192/']
+const profiles = ['https://www.linkedin.com/in/micheleprofmatematica/', 'https://www.linkedin.com/in/dhiogo-bandeira-nobrega-660951192/', 'https://www.linkedin.com/in/conrado-sanchez-alonso']
 // https://www.linkedin.com/in/micheleprofmatematica/
 // https://www.linkedin.com/in/dhiogo-bandeira-nobrega-660951192/
-const profileUrl = profiles[0]
+const profileUrl = profiles[2]
 const loginPage = 'https://www.linkedin.com/authwall?trk=qf&original_referer=https://www.linkedin.com/in/micheleprofmatematica/?originalSubdomain=br&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2F'
 
 describe('template spec', () => {
@@ -18,17 +18,17 @@ describe('template spec', () => {
       cy.get('#session_password').type('Linkedinlti!')
       cy.get('.justify-between > .btn-md').click()
       cy.wait(20000)
-      cy.visit(profileUrl)  
+      cy.visit(profileUrl)
   })
   it('passes', () => {
     cy.wait(2000)
     cy.get('.text-heading-xlarge').invoke('text').then((text) => {
       const title = text.trim();
-      console.log("titulo: ",title) 
+      console.log("titulo: ",title)
     })
     cy.get('.text-body-medium').invoke('text').then((text) => {
       const subscription = text.trim();
-      console.log("descrição: ", subscription) 
+      console.log("descrição: ", subscription)
     })
 
     cy.get('body').then((body) => {
@@ -81,27 +81,34 @@ describe('template spec', () => {
       }
     })
 
-
-    cy.get('body').then((body) => {
-      if (body.find('#navigation-index-see-all-languages').length > 0) {
-        cy.wait(2000)
-        cy.get('#navigation-index-see-all-languages').click()
-      }
-    })
+    //
+    // cy.get('body').then((body) => {
+    //   if (body.find('#navigation-index-see-all-languages').length > 0) {
+    //     cy.wait(2000)
+    //     cy.get('#navigation-index-see-all-languages').click()
+    //   }
+    // })
 
     cy.wait(2000)
 
     cy.get('body').then((body) => {
-      if (body.find('.pvs-list__paged-list-item').length > 0) {
-        cy.get('.pvs-list__paged-list-item').each((lenguages) => {
-          console.log(lenguages[0].children[0].innerText)
+      if (body.find('#languages').length > 0) {
+        cy.get('#languages').then((text) => {
+          const id = text[0].offsetParent.id;
+          if (body.find(`#${id}`)) {
+            if (cy.get(`#${id}`)) {
+              cy.get(`#${id} > :nth-child(3) > :nth-child(1)`).then((value) => {
+                console.log("languages: ", value[0].innerText.trim())
+              })
+            }
+          }
         })
       }
     })
 
 
     cy.wait(2000)
-    cy.visit(profileUrl)  
+    cy.visit(profileUrl)
 
     cy.wait(2000)
 
@@ -134,7 +141,6 @@ describe('template spec', () => {
     cy.get('body').then((body) => {
       if (body.find('.ci-websites > .list-style-none').length > 0) {
         cy.get('.ci-websites > .list-style-none').each((sites) => {
-          console.log(sites)
           if(sites[0].children[0].innerText) {
             array.push(sites[0].children[0].innerText)
           }
@@ -146,25 +152,86 @@ describe('template spec', () => {
       }
     })
 
-    cy.visit(profileUrl)  
+    cy.visit(profileUrl)
     cy.wait(2000)
 
     cy.get('body').then((body) => {
-      if (body.find('#ember134 > :nth-child(3) > :nth-child(1)').length > 0) {
-        cy.get('#ember134 > :nth-child(3) > :nth-child(1)').then((text) => {
-          const academicsFormations = text[0].innerText.trim();
-          console.log("formações academicas", academicsFormations)
-        })
+      if(body.find('#education').length > 0) {
+        if (cy.get('#education')) {
+          cy.get('#education').then((text) => {
+            const id = text[0].offsetParent.id;
+            if (body.find(`#${id}`)) {
+              if (cy.get(`#${id}`)) {
+                cy.get(`#${id} > :nth-child(3) > :nth-child(1)`).then((value) => {
+                  console.log("education: ", value[0].innerText.trim())
+                })
+              }
+            }
+          })
+        }
       }
     })
 
+    // Aguarde até que a página de feed seja carregada
+    // cy.waitUntil(() => cy.url().should('include', '/feed'), { timeout: 10000 });
+
+    // Vá para o perfil do usuário
+    cy.visit(profileUrl);
+
+    // Aguarde até que a página do perfil seja carregada
+    // cy.waitUntil(() => cy.url().should('include', '/in/'), { timeout: 10000 });
+
+    // Role até o final da página para carregar todas as experiências
+
+    cy.wait(2000)
+
+    // cy.get('body').then((body) => {
+    //   if(body.find('#navigation-index-see-all-experiences > .pvs-navigation__text').length > 0) {
+    //     cy.get('#navigation-index-see-all-experiences > .pvs-navigation__text').click()
+    //   }
+    // })
+
+    cy.wait(2000)
+    // cy.get('body').then((body) => {
+    //   if (body.find('.artdeco-list__item').length > 0) {
+    //     if(cy.get('.artdeco-list__item')){
+    //       cy.get('.artdeco-list__item').then((text) => {
+    //         const experiences = text[0].innerText.trim()
+    //         console.log("Experiencias: ", experiences)
+    //       })
+    //     }
+    //   }
+    // })
+
+    // const experiences = [];
+    // cy.get('body').then((body) => {
+    //   if (body.find('li').length > 0) {
+    //     if(cy.get('li')){
+    //       cy.get('li').then((text) => {
+    //         text.each((value) => {
+    //           if(text[value].className.includes("pvs-list__paged-list-item artdeco-list__item pvs-list__item--line-separated pvs-list__item--one-column")){
+    //             experiences.push(text[value].innerText)
+    //           }
+    //           console.log("experiencias ", experiences)
+    //         })
+    //       })
+    //     }
+    //   }
+    // })
+
 
     cy.get('body').then((body) => {
-      if (body.find('.artdeco-list__item').length > 0) {
-        if(cy.get('.artdeco-list__item')){
-          cy.get('.artdeco-list__item').then((text) => {
-            const experiences = text[0].innerText.trim()
-            console.log("Experiencias: ", experiences)
+      if (body.find('#experience').length > 0) {
+        if(cy.get('#experience')){
+          cy.get('#experience').then((text) => {
+            const id = text[0].offsetParent.id;
+            if (body.find(`#${id}`)) {
+              if (cy.get(`#${id}`)) {
+                cy.get(`#${id} > :nth-child(3) > :nth-child(1)`).then((value) => {
+                  console.log("Experience: ", value[0].innerText.trim())
+                })
+              }
+            }
           })
         }
       }
@@ -172,85 +239,195 @@ describe('template spec', () => {
 
 
     cy.get('body').then((body) => {
-      if (body.find('#ember233 > :nth-child(3) > :nth-child(1)').length > 0) {
-        cy.get('#ember233 > :nth-child(3) > :nth-child(1)').then((text) =>{
-          const competences = text[0].innerText.trim();
-          console.log("competencias", competences)
-        })
+      if (body.find('#skills').length > 0) {
+        if(cy.get('#skills')){
+          cy.get('#skills').then((text) => {
+            const id = text[0].offsetParent.id;
+            console.log('id: ', id)
+            if (body.find(`#${id}`)) {
+              if (cy.get(`#${id}`)) {
+                cy.get(`#${id} > :nth-child(3) > :nth-child(1)`).then((value) => {
+                  console.log("skills: ", value[0].innerText.trim())
+
+                })
+              }
+            }
+          })
+        }
+      }
+      // if (body.find('#ember233 > :nth-child(3) > :nth-child(1)').length > 0) {
+      //   cy.get('#ember233 > :nth-child(3) > :nth-child(1)').then((text) =>{
+      //     const competences = text[0].innerText.trim();
+      //     console.log("competencias", competences)
+      //   })
+      // }
+    })
+
+
+    cy.get('body').then((body) => {
+      if (body.find('#volunteering_experience').length > 0) {
+        if(cy.get('#volunteering_experience')){
+          cy.get('#volunteering_experience').then((text) => {
+            const id = text[0].offsetParent.id;
+            if (body.find(`#${id}`)) {
+              if (cy.get(`#${id}`)) {
+                cy.get(`#${id} > :nth-child(3) > :nth-child(1)`).then((value) => {
+                  console.log("Volunteering experience: ", value[0].innerText.trim())
+
+                })
+              }
+            }
+          })
+        }
+
       }
     })
 
 
     cy.get('body').then((body) => {
-      if (body.find('#ember149 > :nth-child(3) > :nth-child(1)').length > 0) {
-        cy.get('#ember149 > :nth-child(3) > :nth-child(1)').then((text) => {
-          const volunteerWork = text[0].innerText.trim();
-          console.log("Trabalhos voluntários: ", volunteerWork)
-        })
+      if (body.find('#licenses_and_certifications').length > 0) {
+        if(cy.get('#licenses_and_certifications')){
+          cy.get('#licenses_and_certifications').then((text) => {
+            const id = text[0].offsetParent.id;
+            if (body.find(`#${id}`)) {
+              if (cy.get(`#${id}`)) {
+                cy.get(`#${id} > :nth-child(3) > :nth-child(1)`).then((value) => {
+                  console.log("licenses and certifications: ", value[0].innerText.trim())
+
+                })
+              }
+            }
+          })
+        }
+      }
+      // if (body.find('#ember152 > :nth-child(3) > :nth-child(1)').length > 0) {
+      //   cy.get('#ember152 > :nth-child(3) > :nth-child(1)').then((text) => {
+      //     const certificates = text[0].innerText.trim()
+      //     console.log("Certificados: ", certificates)
+      //   })
+      // }
+    })
+
+
+    cy.get('body').then((body) => {
+      if (body.find('#projects').length > 0) {
+        if(cy.get('#projects')){
+          cy.get('#projects').then((text) => {
+            const id = text[0].offsetParent.id;
+            if (body.find(`#${id}`)) {
+              if (cy.get(`#${id}`)) {
+                cy.get(`#${id} > :nth-child(3) > :nth-child(1)`).then((value) => {
+                  console.log("projects: ", value[0].innerText.trim())
+
+                })
+              }
+            }
+          })
+        }
       }
     })
 
 
     cy.get('body').then((body) => {
-      if (body.find('#ember152 > :nth-child(3) > :nth-child(1)').length > 0) {
-        cy.get('#ember152 > :nth-child(3) > :nth-child(1)').then((text) => {
-          const certificates = text[0].innerText.trim()
-          console.log("Certificados: ", certificates)
-        })
+      if (body.find('#publications').length > 0) {
+        if(cy.get('#publications')){
+          cy.get('#publications').then((text) => {
+            const id = text[0].offsetParent.id;
+            if (body.find(`#${id}`)) {
+              if (cy.get(`#${id}`)) {
+                cy.get(`#${id} > :nth-child(3) > :nth-child(1)`).then((value) => {
+                  console.log("publications: ", value[0].innerText.trim())
+
+                })
+              }
+            }
+          })
+        }
       }
+      // if (body.find('#ember162 > :nth-child(3) > :nth-child(1)').length > 0) {
+      //   cy.get('#ember162 > :nth-child(3) > :nth-child(1)').then((text) => {
+      //     const publications = text[0].innerText.trim()
+      //     console.log("Publicações: ", publications)
+      //   })
+      // }
     })
 
 
     cy.get('body').then((body) => {
-      if (body.find('#ember156 > :nth-child(3) > :nth-child(1)').length > 0) {
-        cy.get('#ember156 > :nth-child(3) > :nth-child(1)').then((text) => {
-          const projects = text[0].innerText.trim()
-          console.log("Projetos: ", projects)
-        })
+      if (body.find('#courses').length > 0) {
+        if(cy.get('#courses')){
+          cy.get('#courses').then((text) => {
+            const id = text[0].offsetParent.id;
+            if (body.find(`#${id}`)) {
+              if (cy.get(`#${id}`)) {
+                cy.get(`#${id} > :nth-child(3) > :nth-child(1)`).then((value) => {
+                  console.log("courses: ", value[0].innerText.trim())
+
+                })
+              }
+            }
+          })
+        }
       }
+      // if (body.find('#ember163 > .pvs-list__outer-container > .pvs-list > :nth-child(1) > .pvs-entity').length > 0) {
+      //   cy.get('#ember163 > .pvs-list__outer-container > .pvs-list > :nth-child(1) > .pvs-entity').then((text) => {
+      //     const courses = text[0].innerText.trim()
+      //     console.log("Cursos: ", courses)
+      //   })
+      // }
     })
 
 
     cy.get('body').then((body) => {
-      if (body.find('#ember162 > :nth-child(3) > :nth-child(1)').length > 0) {
-        cy.get('#ember162 > :nth-child(3) > :nth-child(1)').then((text) => {
-          const publications = text[0].innerText.trim()
-          console.log("Publicações: ", publications)
-        })
+      if (body.find('#honors_and_awards').length > 0) {
+        if(cy.get('#honors_and_awards')){
+          cy.get('#honors_and_awards').then((text) => {
+            const id = text[0].offsetParent.id;
+            if (body.find(`#${id}`)) {
+              if (cy.get(`#${id}`)) {
+                cy.get(`#${id} > :nth-child(3) > :nth-child(1)`).then((value) => {
+                  console.log("honors and awards: ", value[0].innerText.trim())
+
+                })
+              }
+            }
+          })
+        }
       }
+      // if (body.find('#ember163 > :nth-child(3) > :nth-child(1)').length > 0) {
+      //   cy.get('#ember163 > :nth-child(3) > :nth-child(1)').then((text) => {
+      //     const recognizesAndPrizes = text[0].innerText.trim()
+      //     console.log("Reconhecimentos e premios: ", recognizesAndPrizes)
+      //   })
+      // }
     })
 
 
     cy.get('body').then((body) => {
-      if (body.find('#ember163 > .pvs-list__outer-container > .pvs-list > :nth-child(1) > .pvs-entity').length > 0) {
-        cy.get('#ember163 > .pvs-list__outer-container > .pvs-list > :nth-child(1) > .pvs-entity').then((text) => {
-          const courses = text[0].innerText.trim()
-          console.log("Cursos: ", courses)
-        })
+      if (body.find('#organizations').length > 0) {
+        if(cy.get('#organizations')){
+          cy.get('#organizations').then((text) => {
+            const id = text[0].offsetParent.id;
+            if (body.find(`#${id}`)) {
+              if (cy.get(`#${id}`)) {
+                cy.get(`#${id} > :nth-child(3) > :nth-child(1)`).then((value) => {
+                  console.log("organizations: ", value[0].innerText.trim())
+
+                })
+              }
+            }
+          })
+        }
       }
+      // if (body.find('#ember167 > :nth-child(3) > :nth-child(1)').length > 0) {
+      //   cy.get('#ember167 > :nth-child(3) > :nth-child(1)').then((text) => {
+      //     const organizations = text[0].innerText.trim()
+      //     console.log("Organizações: ", organizations)
+      //   })
+      // }
     })
 
 
-    cy.get('body').then((body) => {
-      if (body.find('#ember163 > :nth-child(3) > :nth-child(1)').length > 0) {
-        cy.get('#ember163 > :nth-child(3) > :nth-child(1)').then((text) => {
-          const recognizesAndPrizes = text[0].innerText.trim()
-          console.log("Reconhecimentos e premios: ", recognizesAndPrizes)
-        })
-      }
-    })
-
-
-    cy.get('body').then((body) => {
-      if (body.find('#ember167 > :nth-child(3) > :nth-child(1)').length > 0) {
-        cy.get('#ember167 > :nth-child(3) > :nth-child(1)').then((text) => {
-          const organizations = text[0].innerText.trim()
-          console.log("Organizações: ", organizations)
-        })
-      }
-    })
-
-    
   })
     // console.log("sites: ")
     // cy.get(':nth-child(1) > .pv-contact-info__contact-link').invoke('text').then((text) => {
@@ -267,11 +444,54 @@ describe('template spec', () => {
     //   const linkedinProfile = text.trim();
     //   console.log("perfil: ", linkedinProfile) 
     // })
-  
+
 })
 
 
 
-
+// describe('Experiências do LinkedIn', () => {
+//   it('Deve obter as experiências do perfil do LinkedIn', () => {
+//     // Visite a página de login do LinkedIn
+//     cy.visit('https://www.linkedin.com/login');
+//
+//     // Preencha os campos de e-mail e senha e faça login
+//     cy.get('#username').type('lticontasc@gmail.com');
+//     cy.get('#password').type('Linkedinlti!');
+//     cy.get('.btn__primary--large').click();
+//
+//     // Aguarde até que a página de feed seja carregada
+//     cy.waitUntil(() => cy.url().should('include', '/feed'), { timeout: 10000 });
+//
+//     // Vá para o perfil do usuário
+//     cy.visit(profileUrl);
+//
+//     // Aguarde até que a página do perfil seja carregada
+//     cy.waitUntil(() => cy.url().should('include', '/in/'), { timeout: 10000 });
+//
+//     // Role até o final da página para carregar todas as experiências
+//     cy.scrollTo('bottom');
+//
+//     // Obtenha todas as seções de experiência do perfil
+//     cy.get('.experience-section').should('have.length.gt', 0).then((sections) => {
+//       // Itere sobre cada seção de experiência
+//       sections.each((index, section) => {
+//         // Obtenha o título da experiência
+//         const title = Cypress.$(section).find('.experience-section__title').text();
+//
+//         // Obtenha as datas de início e fim da experiência
+//         const dates = Cypress.$(section).find('.date-range__duration').text();
+//
+//         // Obtenha a descrição da experiência
+//         const description = Cypress.$(section).find('.experience-section__description').text();
+//
+//         // Imprima as informações da experiência no console
+//         cy.log(`Experiência ${index + 1}`);
+//         cy.log(`Título: ${title}`);
+//         cy.log(`Datas: ${dates}`);
+//         cy.log(`Descrição: ${description}`);
+//       });
+//     });
+//   });
+// });
 
 
